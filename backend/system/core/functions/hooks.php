729400d9050fb33dd4895ce327f6efaea7eca91c
atalapov <atalapov@ukr.net>
2019-01-12 19:42:35 +0200

    update

--- backend/system/core/functions/hooks.php
+++ backend/system/core/functions/hooks.php
@@ -7,39 +7,39 @@
  */
 function add_action($tag, $function_to_add, $priority = 10, $accepted_args = 1){
     global $hooks;
-    return $hooks->add_action($tag, $function_to_add, $priority = 10, $accepted_args = 1);
+    return $hooks->add_action($tag, $function_to_add, $priority, $accepted_args);
 }
 function has_action($tag, $function_to_check = false){
     global $hooks;
-    return $hooks->has_action($tag, $function_to_check = false);
+    return $hooks->has_action($tag, $function_to_check);
 }
 function remove_action( $tag, $function_to_remove, $priority = 10 ){
     global $hooks;
-    return $hooks->remove_action( $tag, $function_to_remove, $priority = 10 );
+    return $hooks->remove_action( $tag, $function_to_remove, $priority);
 }
 function remove_all_actions($tag, $priority = false){
     global $hooks;
-    return $hooks->remove_all_actions($tag, $priority = false);
+    return $hooks->remove_all_actions($tag, $priority);
 }
 function do_action($tag, $arg = ''){
     global $hooks;
-    return $hooks->do_action($tag, $arg = '');
+    return $hooks->do_action($tag, $arg);
 }
 function add_filter($tag, $function_to_add, $priority = 10, $accepted_args = 1){
     global $hooks;
-    return $hooks->add_filter($tag, $function_to_add, $priority = 10, $accepted_args = 1);
+    return $hooks->add_filter($tag, $function_to_add, $priority, $accepted_args);
 }
 function remove_filter( $tag, $function_to_remove, $priority = 10 ){
     global $hooks;
-    return $hooks->remove_filter( $tag, $function_to_remove, $priority = 10 );
+    return $hooks->remove_filter( $tag, $function_to_remove, $priority);
 }
 function remove_all_filters($tag, $priority = false){
     global $hooks;
-    return $hooks->remove_all_filters($tag, $priority = false);
+    return $hooks->remove_all_filters($tag, $priority);
 }
 function has_filter($tag, $function_to_check = false){
     global $hooks;
-    return $hooks->has_filter($tag, $function_to_check = false);
+    return $hooks->has_filter($tag, $function_to_check);
 }
 function apply_filters($tag, $value){
     global $hooks;
